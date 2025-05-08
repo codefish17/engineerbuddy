63bfd7acf71400cd2f751b1d8e53d20f5d34cb34
codefish17 <housefish17@proton.me>
2025-05-08 14:22:35 -0400

    Job class has CRD operations, working on U

--- brain.py
+++ brain.py
@@ -1,5 +1,27 @@
+import sqlite3
+from model import job
+from model import task
+
 class Brain:
-    pass
+
+    def __init__(self):
+        self.db_connection = sqlite3.connect("model\\test.db")
 
 if __name__ == "__main__":
-    pass
+    my_brain = Brain()
+
+    # j = job.Job()
+    # j.name = "testrow2"
+    # j.CreateJob(my_brain.db_connection)
+
+    cur = my_brain.db_connection.cursor()
+
+    res = cur.execute("PRAGMA table_xinfo(Jobs)")
+
+    print(res.fetchall())
+
+    my_brain.db_connection.close()
+
+    
+
+
--- model/job.py
+++ model/job.py
@@ -1,24 +1,65 @@
 import sqlite3
 
 class Job:
-    id = ""
-    name = ""
-    description = ""
-    date_created = ""
-    date_due = ""
-    tasks = []
 
-    def CreateJob(self):
-        pass
+    table_columns = ["id", "name", "description", "requestor", "date_created", "date_due"]
+
+    def __init__(self, id = "", name = "", description ="", requestor = "", date_created = "", date_due = "", is_complete=False, tasks = []):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.requestor = requestor
+        self.date_created = date_created
+        self.date_due = date_due
+        self.is_complete = is_complete
+        self.tasks = tasks
+
+    def CreateJob(self, connection):
+        cur = connection.cursor()
+        params = (self.name, self.description, self.requestor, self.date_created, self.date_due)
+        sql_str = """INSERT INTO Jobs (name, description, requestor, date_created, date_due) VALUES (?,?,?,?,?)"""
+        cur.execute(sql_str, params)
+        connection.commit()
+
+    @staticmethod 
+    def GetJob(connection, id) -> list:
+        cur = connection.cursor()
+        res = cur.execute("SELECT * FROM Jobs WHERE id=?", (id,))
+
+        return res.fetchall()
     
-    def GetJobs(self):
-        pass
+    @staticmethod
+    def GetJobs(connection) -> list:
+        cur = connection.cursor()
+        res = cur.execute("SELECT * FROM Jobs")
 
-    def UpdateJob(self):
+        return res.fetchall()
+    
+    @staticmethod
+    def UpdateJob(connection, id, column, value):
         pass
-
-    def DeleteJob(self):
-        pass
+        
+    @staticmethod
+    def DeleteJob(connection, id):
+        cur = connection.cursor()
+        cur.execute("DELETE FROM Jobs WHERE id=?", (id,))
+        connection.commit()
 
 if __name__ == "__main__":
-    pass
+    con = sqlite3.connect('model\\test.db')
+    cur = con.cursor()
+    
+    # con.execute("DROP TABLE Jobs")
+
+    # con.execute("""CREATE TABLE Jobs(
+    #             id INTEGER PRIMARY KEY NOT NULL,
+    #             name TEXT UNIQUE NOT NULL,
+    #             description TEXT,
+    #             requestor TEXT,
+    #             date_created TEXT,
+    #             date_due TEXT)""")
+    
+    con.commit()
+    con.close()
+
+
