3afc88aaa9695ed312f297496bcd95d34b0310e0
codefish17 <housefish17@proton.me>
2025-05-08 09:14:05 -0400

    before adding database to allow ignoring of future .db files

---
+++ model/job.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class Job:
+    id = ""
+    name = ""
+    description = ""
+    date_created = ""
+    date_due = ""
+    tasks = []
+
+    def CreateJob(self):
+        pass
+    
+    def GetJobs(self):
+        pass
+
+    def UpdateJob(self):
+        pass
+
+    def DeleteJob(self):
+        pass
+
+if __name__ == "__main__":
+    pass
---
+++ model/task.py
@@ -0,0 +1,5 @@
+class Task:
+    pass
+
+if __name__ == "__main__":
+    pass
